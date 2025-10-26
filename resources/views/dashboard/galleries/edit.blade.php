b38a2e632ea9632d48fb5b429c081fb6081c89ae
Beshoy <beshoy.ecladuos@onexpand.co.uk>
2025-10-26 13:12:22 +0300

    working on the folder validation 5

--- resources/views/dashboard/galleries/edit.blade.php
+++ resources/views/dashboard/galleries/edit.blade.php
@@ -1,11 +1,8 @@
 @extends('adminlte::page')
 @section('title', 'Update Gallery')
 
-@section('content_header')
-    <h1>Update Gallery</h1>
-@stop
-
 @section('content')
+    <h4 class="page_header">Update Gallery</h4>
     <form method="POST" action="{{ route('dashboard.galleries.update', $gallery->id) }}" enctype="multipart/form-data">
         @method('PUT')
         <div class="ibox-content">
