907d4f509fb7e5da632519269b4de387e08e9539
Beshoy <beshoy.ecladuos@onexpand.co.uk>
2025-10-30 02:31:08 +0300

    adding the functionality to upload fies

--- resources/views/dashboard/folders/index.blade.php
+++ resources/views/dashboard/folders/index.blade.php
@@ -13,13 +13,11 @@
 
             <div class="col-sm-8 col-md-9 ibox-content" :class="{ 'col-sm-12 col-md-12': sidebarCollapsed }">
 
-
-
                 <!-- Collapse/Expand Button -->
                 <div style="position: absolute;top: -10px;left: -5px;">
-                    <button style="border-radius: 50%;" class="btn btn-sm btn-secondary" @click="toggleSidebar">
-                        <i v-if="sidebarCollapsed" class="fas fa-step-forward"></i>
-                        <i v-else class="fas fa-step-backward"></i>
+                    <button class="btn btn-sm btn-secondary" @click="toggleSidebar">
+                        <i v-if="sidebarCollapsed" class="fas fa-chevron-right"></i>
+                        <i v-else class="fas fa-chevron-left"></i> 
                     </button>
                 </div>
 
@@ -37,15 +35,8 @@
                     <span>Please select a folder from the list to upload files</span>
                 </div>
 
-
-
-                {{-- <div 
-                    class="dropzone" data-gallery-id="{{ $gallery->id }}" :data-folder-id="currentFolderId">
-                </div> --}}
-
                 <dropzone-uploader :gallery-id="{{ $gallery->id }}" :current-folder-id="currentFolderId"></dropzone-uploader>
 
-                
             </div>
         </div>
     </div>
@@ -60,12 +51,6 @@
             color: #646c7f;
             font-size: 20px;
             margin-top: 82px;
-        }
-        .col-sm-4.col-md-3 {
-            /* transition: all 0.3s ease; */
-        }
-        .ibox-content {
-            /* transition: all 0.3s ease; */
         }
     </style>
 @stop
